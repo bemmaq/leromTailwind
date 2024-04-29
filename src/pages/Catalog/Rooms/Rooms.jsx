67dc618e1be2e4c62e71a9416fcc31b202fe36d3
Begimai <bahadurybabegimai@gmail.com>
2024-04-29 11:09:02 +0600

    rooms

--- src/pages/Catalog/Rooms/Rooms.jsx
+++ src/pages/Catalog/Rooms/Rooms.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Container from "../../../Helpers/Container";
+import Catalog from '../../../components/Catalog/Catalog';
+import RoomProduct from './RoomProduct/RoomProduct';
+import RoomBlock from './RoomBlock/RoomBlock';
+import RoomsSale from './RoomsSale/RoomsSale';
 
 const Rooms = () => {
    
   return (
-    <div>Rooms </div>
+    <div className="mt-[30px] font-semibold">
+      <Container>
+      <div className=" border-2 leading-[50px] w-[200px] absolute  z-10 rounded-lg  h-[160px]  shadow-lg shadow-black-500/50 font-semibold bg-white">
+        <Catalog/>
+      </div>
+      <RoomProduct/>
+      <RoomBlock/>
+      <RoomsSale/>
+      </Container>
+    </div>
   )
 }
 
---
+++ src/pages/Catalog/Rooms/RoomsSale/RoomsSale.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sale from '../../New/Sale/Sale'
+
+const RoomsSale = () => {
+  return (
+    <div>
+        <Sale/>
+    </div>
+  )
+}
+
+export default RoomsSale

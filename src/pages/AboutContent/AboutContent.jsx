a3e53b435df74b267cbefdea47a5dda68c54f244
Begimai <bahadurybabegimai@gmail.com>
2024-04-29 11:49:00 +0600

    about

---
+++ src/pages/AboutContent/AboutContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutContent = () => {
+  return (
+    <div className='font-semibold pt-5'>
+        <h2 className='text-3xl'>Скачать на компьютер, 606 Мб</h2>
+        <button className="bg-red-500 text-white w-[200px] h-[60px] rounded-md ml-[500px] mt-20 text-center">Загрузить программу</button>
+    </div>
+  )
+}
+
+export default AboutContent

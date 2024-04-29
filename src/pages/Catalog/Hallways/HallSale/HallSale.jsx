5d85a05a4868e4518505e8b0b4d269cab4701d95
Begimai <bahadurybabegimai@gmail.com>
2024-04-29 11:20:45 +0600

    hallways

---
+++ src/pages/Catalog/Hallways/HallSale/HallSale.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sale from '../../New/Sale/Sale'
+
+const HallSale = () => {
+  return (
+    <div>
+        <Sale/>
+    </div>
+  )
+}
+
+export default HallSale
--- src/pages/Catalog/Hallways/Hallways.jsx
+++ src/pages/Catalog/Hallways/Hallways.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Container from '../../../Helpers/Container'
+import Catalog from '../../../components/Catalog/Catalog'
+import HallwaysProduct from './HallwaysProduct/HallwaysProduct'
+import HallSale from './HallSale/HallSale'
+import HallBlock from './HallBlock/HallBlock'
 
 const Hallways = () => {
     
   return (
-    <div>Hallways </div>
+    <div className="mt-[30px] font-semibold">
+      <div>
+        <Container>
+        <div className=" border-2 leading-[50px] w-[200px] absolute  z-10 rounded-lg  h-[160px]  shadow-lg shadow-black-500/50 font-semibold bg-white">
+          <Catalog />
+        </div>
+        <HallwaysProduct/>
+        <HallSale/>
+        <HallBlock/>
+        </Container>
+      </div>
+       </div>
   )
 }
 

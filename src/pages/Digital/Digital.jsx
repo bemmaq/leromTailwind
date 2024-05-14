b19a80e5054b9a7d180ca13dc1905f21fc271b87
Begimai <bahadurybabegimai@gmail.com>
2024-05-14 13:08:30 +0600

    digitall

--- src/pages/Digital/Digital.jsx
+++ src/pages/Digital/Digital.jsx
@@ -4,7 +4,7 @@ import esv from '../../assets/Demos Pages Support.svg'
 import { Link } from 'react-router-dom'
 const Digital = () => {
   return (
-    <div className='flex justify-between mt-10'>
+    <div className='flex justify-between mt-10 px-10'>
         <p className='text-gray-300 pt-5'>©  ООО "Мебельная <br /> компания "Лером" 2021   </p>
         <Link to={'/'}>
         <img src={logo} alt="" />
